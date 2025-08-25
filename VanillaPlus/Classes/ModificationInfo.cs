20608f1e0bb7a6e4878b9d65b12c1129c79109f1
MidoriKami <mi_kami@outlook.com>
2025-08-25 16:36:48 -0700

    Fix typo

--- VanillaPlus/Classes/ModificationInfo.cs
+++ VanillaPlus/Classes/ModificationInfo.cs
@@ -15,10 +15,10 @@ public class ModificationInfo {
     public List<string> Tags { get; init; } = [];
     
     /// <summary>
-    /// Compatability Module prevents loading this GameModification if the
+    /// Compatibility Module prevents loading this GameModification if the
     /// associated plugin has the equivalent module enabled.
     /// </summary>
-    public CompatabilityModule? CompatabilityModule { get; init; }
+    public CompatibilityModule? CompatibilityModule { get; init; }
 
     public bool IsMatch(string searchTerm) {
         if (DisplayName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)) return true;
