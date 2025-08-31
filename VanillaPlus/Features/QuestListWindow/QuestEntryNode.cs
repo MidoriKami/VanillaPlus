026b86492c0a18910cafc6de610a1c51066445f7
MidoriKami <mi_kami@outlook.com>
2025-08-31 13:41:44 -0700

    Add issuer and distance nodes

--- VanillaPlus/Features/QuestListWindow/QuestEntryNode.cs
+++ VanillaPlus/Features/QuestListWindow/QuestEntryNode.cs
@@ -3,6 +3,7 @@ using Dalamud.Game.Addon.Events;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
 using KamiToolKit.Nodes;
 
 namespace VanillaPlus.Features.QuestListWindow;
@@ -13,6 +14,8 @@ public unsafe class QuestEntryNode : SimpleComponentNode {
     private readonly IconImageNode questIconNode;
     private readonly TextNode questNameTextNode;
     private readonly TextNode questLevelTextNode;
+    private readonly TextNode issuerNameTextNode;
+    private readonly TextNode distanceTextNode;
 
     public QuestEntryNode() {
         hoveredBackgroundNode = new SimpleNineGridNode {
@@ -36,18 +39,38 @@ public unsafe class QuestEntryNode : SimpleComponentNode {
 
         questNameTextNode = new TextNode {
             NodeId = 4,
-            AlignmentType = AlignmentType.Left,
+            AlignmentType = AlignmentType.BottomLeft,
             TextFlags = TextFlags.Ellipsis,
+            FontSize = 13,
             IsVisible = true,
         };
         System.NativeController.AttachNode(questNameTextNode, this);
 
-        questLevelTextNode = new TextNode {
+        issuerNameTextNode = new TextNode {
             NodeId = 5,
-            AlignmentType = AlignmentType.Left,
+            AlignmentType = AlignmentType.TopLeft,
+            TextColor = ColorHelper.GetColor(2),
+            TextFlags = TextFlags.Ellipsis,
+            FontSize = 12,
+            IsVisible = true,
+        };
+        System.NativeController.AttachNode(issuerNameTextNode, this);
+        
+        questLevelTextNode = new TextNode {
+            NodeId = 6,
+            AlignmentType = AlignmentType.BottomLeft,
+            FontSize = 13,
             IsVisible = true,
         };
         System.NativeController.AttachNode(questLevelTextNode, this);
+
+        distanceTextNode = new TextNode {
+            NodeId = 7,
+            AlignmentType = AlignmentType.TopRight,
+            TextColor = ColorHelper.GetColor(2),
+            IsVisible = true,
+        };
+        System.NativeController.AttachNode(distanceTextNode, this);
         
         CollisionNode.AddEvent(AddonEventType.MouseOver, _ => {
             IsHovered = true;
@@ -57,9 +80,9 @@ public unsafe class QuestEntryNode : SimpleComponentNode {
             if (QuestInfo is null) return;
 
             var agentMap = AgentMap.Instance();
-            agentMap->OpenMap(agentMap->CurrentMapId, agentMap->CurrentTerritoryId);
+            agentMap->OpenMap(agentMap->CurrentMapId, agentMap->CurrentTerritoryId, QuestInfo.Name.ToString());
             agentMap->FlagMarkerCount = 0;
-            agentMap->SetFlagMapMarker(agentMap->CurrentTerritoryId, agentMap->CurrentMapId, QuestInfo.MarkerData.Position, QuestInfo.MarkerData.IconId);
+            agentMap->SetFlagMapMarker(agentMap->CurrentTerritoryId, agentMap->CurrentMapId, QuestInfo.Position, QuestInfo.IconId);
             RaptureAtkModule.Instance()->FocusAddon(agentMap->AddonId);
         });
         
@@ -82,6 +105,7 @@ public unsafe class QuestEntryNode : SimpleComponentNode {
             
             questIconNode.IconId = value.IconId;
             questNameTextNode.ReadOnlySeString = value.Name;
+            issuerNameTextNode.ReadOnlySeString = value.IssuerName;
         }
     }
 
@@ -98,10 +122,19 @@ public unsafe class QuestEntryNode : SimpleComponentNode {
         questIconNode.Size = new Vector2(Height, Height);
         questIconNode.Position = Vector2.Zero;
 
-        questLevelTextNode.Size = new Vector2(45.0f, Height);
+        questLevelTextNode.Size = new Vector2(45.0f, Height / 2.0f);
         questLevelTextNode.Position = new Vector2(Width - questLevelTextNode.Width - 4.0f, 0.0f);
         
-        questNameTextNode.Size = new Vector2(Width - questIconNode.Width - questLevelTextNode.Width - 8.0f, Height);
+        questNameTextNode.Size = new Vector2(Width - questIconNode.Width - questLevelTextNode.Width - 8.0f, Height / 2.0f);
         questNameTextNode.Position = new Vector2(questIconNode.Width + 4.0f, 0.0f);
+
+        issuerNameTextNode.Size = new Vector2(Width - questIconNode.Width - questLevelTextNode.Width - 16.0f, Height / 2.0f);
+        issuerNameTextNode.Position = new Vector2(questIconNode.Width + 12.0f, Height / 2.0f);
+
+        distanceTextNode.Size = questLevelTextNode.Size;
+        distanceTextNode.Position = new Vector2(Width - questLevelTextNode.Width - 4.0f, Height / 2.0f);
     }
+
+    public void Update()
+        => distanceTextNode.String = $"{QuestInfo.Distance:F1} y";
 }
