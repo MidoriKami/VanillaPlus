4464e44946c8a3514d59f14ae22e6c6fc24037c0
MidoriKami <mi_kami@outlook.com>
2025-10-20 15:28:22 -0700

    formatting

--- VanillaPlus/NativeElements/Config/ConfigEntries/IntSliderConfig.cs
+++ VanillaPlus/NativeElements/Config/ConfigEntries/IntSliderConfig.cs
@@ -26,7 +26,6 @@ public class IntSliderConfig : BaseConfigEntry {
             Value = InitialValue,
         };
 
-
         var labelNode = new SimpleLabelNode {
             IsVisible = true,
             Size = new Vector2(0.0f, 24.0f),
