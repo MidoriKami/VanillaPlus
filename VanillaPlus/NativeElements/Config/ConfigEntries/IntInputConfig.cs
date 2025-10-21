a5fd262f785e76756c4aef91937dd1a5703aa0b3
MidoriKami <mi_kami@outlook.com>
2025-10-20 18:43:16 -0700

    Add InputIntConfig

---
+++ VanillaPlus/NativeElements/Config/ConfigEntries/IntInputConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+using KamiToolKit.Nodes;
+using KamiToolKit.System;
+
+namespace VanillaPlus.NativeElements.Config.ConfigEntries;
+
+public class IntInputConfig : BaseConfigEntry {
+    public required int InitialValue { get; init; }
+    public required int Step { get; init; }
+    public required Range Range { get; init; }
+
+    public override NodeBase BuildNode() {
+        var layoutNode = new HorizontalListNode {
+            Height = 24.0f,
+            IsVisible = true,
+            ItemSpacing = 10.0f,
+        };
+
+        var numericInput = new NumericInputNode {
+            Size = new Vector2(100.0f, 24.0f),
+            Value = InitialValue,
+            IsVisible = true,
+            Step = Step,
+            Min = Range.Start.Value,
+            Max = Range.End.Value,
+            OnValueUpdate = newValue => {
+                MemberInfo.SetValue(Config, newValue);
+                Config.Save();
+            },
+        };
+
+        layoutNode.AddNode(numericInput);
+        layoutNode.AddNode(GetLabelNode());
+        
+        return layoutNode;
+    }
+}
