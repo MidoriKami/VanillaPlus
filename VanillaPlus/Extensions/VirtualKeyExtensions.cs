9471397ffbf71b950286b6df6de37d3f36b6c24c
MidoriKami <mi_kami@outlook.com>
2025-10-24 02:55:35 -0700

    Add VirtualKeyExtensions

---
+++ VanillaPlus/Extensions/VirtualKeyExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Dalamud.Game.ClientState.Keys;
+
+namespace VanillaPlus.Extensions;
+
+public static class VirtualKeyExtensions {
+    public static bool IsModifier(this VirtualKey key)
+        => key is VirtualKey.MENU or VirtualKey.SHIFT or VirtualKey.CONTROL;
+
+    public static bool IsKey(this VirtualKey key)
+        => !key.IsModifier();
+}
