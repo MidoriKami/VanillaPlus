928a8541f02680d1a2ee8459043daafa3b16d332
MidoriKami <mi_kami@outlook.com>
2025-10-24 02:55:44 -0700

    Add helpers to Keybind

--- VanillaPlus/Classes/Keybind.cs
+++ VanillaPlus/Classes/Keybind.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Dalamud.Game.ClientState.Keys;
 
 namespace VanillaPlus.Classes;
@@ -6,4 +7,13 @@ namespace VanillaPlus.Classes;
 public class Keybind {
     public VirtualKey Key { get; set; } = VirtualKey.NO_KEY;
     public HashSet<VirtualKey> Modifiers { get; set; } = [];
+
+    public bool IsPressed()
+        => Services.KeyState.IsKeybindPressed(Modifiers.Append(Key));
+
+    public void Reset()
+        => Services.KeyState.ResetKeyCombo([ Key ] );
+
+    public override string ToString()
+        => string.Join(" + ", Modifiers.Append(Key));
 }
