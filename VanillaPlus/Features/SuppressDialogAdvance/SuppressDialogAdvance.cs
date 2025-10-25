7b95f7b05e4756d63b06a380dc8b35d9c967f921
MidoriKami <mi_kami@outlook.com>
2025-10-25 10:26:09 -0700

    Add ApplyOnlyInCutscene to SuppressDialogAdvance

--- VanillaPlus/Features/SuppressDialogAdvance/SuppressDialogAdvance.cs
+++ VanillaPlus/Features/SuppressDialogAdvance/SuppressDialogAdvance.cs
@@ -3,6 +3,7 @@ using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using VanillaPlus.Classes;
+using VanillaPlus.NativeElements.Config;
 
 namespace VanillaPlus.Features.SuppressDialogAdvance;
 
@@ -14,17 +15,43 @@ public unsafe class SuppressDialogueAdvance : GameModification {
         Authors = [ "MidoriKami" ],
         ChangeLog = [
             new ChangeLogInfo(1, "InitialChangelog"),
+            new ChangeLogInfo(2, "Added option to only apply while in a cutscene, enabled by default."),
         ],
     };
 
-    public override void OnEnable()
-        => Services.AddonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, "Talk", OnTalkReceiveEvent);
+    private SuppressDialogAdvanceConfig? config;
+    private ConfigAddon? configWindow;
 
-    public override void OnDisable()
-        => Services.AddonLifecycle.UnregisterListener(OnTalkReceiveEvent);
+    public override void OnEnable() {
+        config = SuppressDialogAdvanceConfig.Load();
 
-    private static void OnTalkReceiveEvent(AddonEvent type, AddonArgs args) {
+        configWindow = new ConfigAddon {
+            NativeController = System.NativeController,
+            InternalName = "SuppressDialogAdvanceConfig",
+            Title = "Suppress Dialog Advance Config",
+            Config = config,
+        };
+        
+        configWindow.AddCategory("General")
+            .AddCheckbox("Apply only in Cutscenes", nameof(config.ApplyOnlyInCutscenes));
+
+        OpenConfigAction = configWindow.Toggle;
+        
+        Services.AddonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, "Talk", OnTalkReceiveEvent);
+    }
+
+    public override void OnDisable() {
+        Services.AddonLifecycle.UnregisterListener(OnTalkReceiveEvent);
+
+        configWindow?.Dispose();
+        configWindow = null;
+        
+        config = null;
+    }
+
+    private void OnTalkReceiveEvent(AddonEvent type, AddonArgs args) {
         if (args is not AddonReceiveEventArgs eventArgs) return;
+        if ((config?.ApplyOnlyInCutscenes ?? false) && !Services.Condition.IsInCutscene()) return;
 
         if ((AtkEventType)eventArgs.AtkEventType is AtkEventType.MouseClick) {
             var addon = args.GetAddon<AddonTalk>();
---
+++ VanillaPlus/Features/SuppressDialogAdvance/SuppressDialogAdvanceConfig.cs
@@ -0,0 +1,9 @@
+﻿using VanillaPlus.Classes;
+
+namespace VanillaPlus.Features.SuppressDialogAdvance;
+
+public class SuppressDialogAdvanceConfig : GameModificationConfig<SuppressDialogAdvanceConfig> {
+    protected override string FileName => "SuppressDialogAdvance.config.json";
+
+    public bool ApplyOnlyInCutscenes = true;
+}
