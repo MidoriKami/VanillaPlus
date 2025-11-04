d4b8eba83f1e110d5ffce2c87b7c272ca4e7cfc7
MidoriKami <mi_kami@outlook.com>
2025-11-03 20:13:56 -0800

    Add public general purpose Territory and Gearset Search Addons

---
+++ VanillaPlus/NativeElements/Addons/SearchAddons/GearsetSearchAddon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using KamiToolKit.Addons;
+using VanillaPlus.Features.GearsetRedirect;
+
+namespace VanillaPlus.NativeElements.Addons.SearchAddons;
+
+public static unsafe class GearsetSearchAddon {
+    public static SearchAddon<GearsetInfo> GetAddon() => new() {
+        NativeController = System.NativeController,
+        Size = new Vector2(275.0f, 600.0f),
+        InternalName = "GearsetSearch",
+        Title = "Gearset Search",
+        SearchOptions = [],
+        SortingOptions = [ "Alphabetical", "Id" ],
+    };
+
+    public static void UpdateGearsets(this SearchAddon<GearsetInfo> addon, List<int>? omissionIds = null)
+        => addon.SearchOptions = GetGearsetInfos(omissionIds ?? []);
+
+    private static List<GearsetInfo> GetGearsetInfos(List<int> omissionIds) {
+        List<GearsetInfo> entries = [];
+
+        foreach (ref var gearset in RaptureGearsetModule.Instance()->Entries) {
+            if (gearset.NameString.IsNullOrEmpty()) continue;
+            if (omissionIds.Contains(gearset.Id)) continue;
+        
+            entries.Add(new GearsetInfo {
+                GearsetId = gearset.Id,
+            });
+        }
+
+        return entries;
+    }
+}
---
+++ VanillaPlus/NativeElements/Addons/SearchAddons/TerritorySearchAddon.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Numerics;
+using Dalamud.Utility;
+using KamiToolKit.Addons;
+using Lumina.Excel.Sheets;
+
+namespace VanillaPlus.NativeElements.Addons.SearchAddons;
+
+public static class TerritorySearchAddon {
+    public static LuminaSearchAddon<TerritoryType> GetAddon() => new () {
+        NativeController = System.NativeController,
+        Size = new Vector2(350.0f, 600.0f),
+        InternalName = "TerritorySearch",
+        Title = "Zone Search",
+        SearchOptions = Services.DataManager.GetExcelSheet<TerritoryType>()
+            .Where(territory => territory.LoadingImage.RowId is not 0)
+            .Where(territory => !territory.PlaceName.Value.Name.ToString().IsNullOrEmpty())
+            .ToList(),
+
+        GetLabelFunc = territory => territory.PlaceName.Value.Name.ToString(),
+        GetIconIdFunc = _ => 60072,
+        GetTexturePathFunc = territory => $"ui/loadingimage/{territory.LoadingImage.Value.FileName}_hr1.tex",
+        GetSubLabelFunc = territory => territory.ContentFinderCondition.RowId is 0 ? string.Empty : territory.ContentFinderCondition.Value.Name.ToString(),
+
+        SortingOptions = [ "Alphabetical" , "Id" ],
+    };
+}
