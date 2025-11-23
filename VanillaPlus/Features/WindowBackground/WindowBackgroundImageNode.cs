2503d3b3ac19cf6adbe154b1b63b0ff7076812e3
MidoriKami <mi_kami@outlook.com>
2025-11-22 21:40:20 -0800

    Add new experimental node

---
+++ VanillaPlus/Features/WindowBackground/WindowBackgroundImageNode.cs
@@ -0,0 +1,31 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Classes.Controllers.Overlay;
+using KamiToolKit.Nodes;
+
+namespace VanillaPlus.Features.WindowBackground;
+
+public unsafe class WindowBackgroundImageNode : OverlayNode {
+
+    public override OverlayLayer OverlayLayer => OverlayLayer.BackLayer;
+    
+    public required WindowBackgroundSetting Settings { get; init; }
+    
+    private readonly BackgroundImageNode backgroundImageNode;
+
+    public WindowBackgroundImageNode() {
+        backgroundImageNode = new BackgroundImageNode();
+        backgroundImageNode.AttachNode(this);
+    }
+    
+    public override void Update() {
+        var addon = Services.GameGui.GetAddonByName<AtkUnitBase>(Settings.AddonName);
+        backgroundImageNode.IsVisible = addon is not null && addon->IsActuallyVisible();
+
+        if (addon is not null) {
+            backgroundImageNode.Color = Settings.Color;
+            backgroundImageNode.Position = addon->Position() - Settings.Padding / 2.0f;
+            backgroundImageNode.Size = (addon->RootSize() + Settings.Padding) * addon->Scale;
+        }
+    }
+}
