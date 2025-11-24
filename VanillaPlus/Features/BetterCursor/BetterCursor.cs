02974b2362851fac552f28bc6b2dd1998b416bbf
MidoriKami <mi_kami@outlook.com>
2025-11-23 17:47:34 -0800

    Use Native Overlays for BetterCursor

--- VanillaPlus/Features/BetterCursor/BetterCursor.cs
+++ VanillaPlus/Features/BetterCursor/BetterCursor.cs
@@ -1,18 +1,12 @@
 ﻿using System.Drawing;
-using System.Numerics;
 using Dalamud.Interface;
-using FFXIVClientStructs.FFXIV.Client.System.Input;
-using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes.Controllers;
-using KamiToolKit.Classes.Timelines;
-using KamiToolKit.Nodes;
 using VanillaPlus.Classes;
 using VanillaPlus.NativeElements.Config;
 
 namespace VanillaPlus.Features.BetterCursor;
 
-public unsafe class BetterCursor : GameModification {
+public class BetterCursor : GameModification {
     public override ModificationInfo ModificationInfo => new() {
         DisplayName = "Better Cursor",
         Description = "Draws a ring around the cursor to make it easier to see",
@@ -25,10 +19,7 @@ public unsafe class BetterCursor : GameModification {
         ],
     };
 
-    private ResNode? animationContainer;
-    private IconImageNode? imageNode;
-
-    private AddonController<AtkUnitBase>? screenTextController;
+    private OverlayController? overlayController;
 
     private BetterCursorConfig? config;
     private ConfigAddon? configWindow;
@@ -59,106 +50,24 @@ public unsafe class BetterCursor : GameModification {
         configWindow.AddCategory("Icon Selection")
             .AddSelectIcon("Icon", nameof(config.IconId));
 
-        config.OnSave += UpdateNodeConfig;
-
         OpenConfigAction = configWindow.Toggle;
 
-        screenTextController = new AddonController<AtkUnitBase>("_ScreenText");
-        screenTextController.OnAttach += AttachNodes;
-        screenTextController.OnDetach += DetachNodes;
-        screenTextController.OnUpdate += Update;
-        screenTextController.Enable();
+        overlayController = new OverlayController();
+
+        Services.Framework.RunOnFrameworkThread(() => {
+            overlayController.AddNode(new CursorImageNode {
+                Config = config,
+            });
+        });
     }
 
     public override void OnDisable() {
-        screenTextController?.Dispose();
-        screenTextController = null;
+        overlayController?.Dispose();
+        overlayController = null;
         
         configWindow?.Dispose();
         configWindow = null;
         
         config = null;
-    }
-
-    private void UpdateNodeConfig() {
-        if (config is null) return;
-        
-        if (animationContainer is not null) {
-            animationContainer.Size = new Vector2(config.Size);
-        }
-
-        if (imageNode is not null) {
-            imageNode.Size = new Vector2(config.Size);
-            imageNode.Origin = new Vector2(config.Size / 2.0f);
-            imageNode.Color = config.Color;
-            imageNode.IconId = config.IconId;
-        }
-
-        animationContainer?.Timeline?.PlayAnimation(config.Animations ? 1 : 2);
-    }
-
-    private void Update(AtkUnitBase* addon) {
-        if (config is null) return;
-
-        if (animationContainer is not null && imageNode is not null) {
-            ref var cursorData = ref UIInputData.Instance()->CursorInputs;
-            animationContainer.Position = new Vector2(cursorData.PositionX, cursorData.PositionY) - imageNode.Size / 2.0f;
-
-            var isLeftHeld = (cursorData.MouseButtonHeldFlags & MouseButtonFlags.LBUTTON) != 0;
-            var isRightHeld = (cursorData.MouseButtonHeldFlags & MouseButtonFlags.RBUTTON) != 0;
-
-            if (config is { OnlyShowInCombat: true } or { OnlyShowInDuties: true }) {
-                var shouldShow = true;
-                shouldShow &= !config.OnlyShowInCombat || Services.Condition.IsInCombat();
-                shouldShow &= !config.OnlyShowInDuties || Services.Condition.IsBoundByDuty();
-                shouldShow &= !config.HideOnCameraMove || (!isLeftHeld && !isRightHeld);
-
-                animationContainer.IsVisible = shouldShow;
-            }
-            else {
-                animationContainer.IsVisible = !isLeftHeld && !isRightHeld || !config.HideOnCameraMove;
-            }
-        }
-    }
-
-    private void AttachNodes(AtkUnitBase* addon) {
-        animationContainer = new ResNode();
-        animationContainer.AttachNode(addon);
-
-        imageNode = new IconImageNode {
-            IconId = 60498,
-            FitTexture = true,
-        };
-        imageNode.AttachNode(animationContainer);
-
-        animationContainer.AddTimeline(new TimelineBuilder()
-            .BeginFrameSet(1, 120)
-            .AddLabel(1, 1, AtkTimelineJumpBehavior.Start, 0)
-            .AddLabel(60, 0, AtkTimelineJumpBehavior.LoopForever, 1)
-            .AddLabel(61, 2, AtkTimelineJumpBehavior.Start, 0)
-            .AddLabel(120, 0, AtkTimelineJumpBehavior.LoopForever, 2)
-            .EndFrameSet()
-            .Build());
-
-        imageNode.AddTimeline(new TimelineBuilder()
-            .BeginFrameSet(1, 60)
-            .AddFrame(1, scale: new Vector2(1.0f, 1.0f))
-            .AddFrame(30, scale: new Vector2(0.75f, 0.75f))
-            .AddFrame(60, scale: new Vector2(1.0f, 1.0f))
-            .EndFrameSet()
-            .BeginFrameSet(61, 120)
-            .AddFrame(61, scale: new Vector2(1.0f, 1.0f))
-            .EndFrameSet()
-            .Build());
-
-        UpdateNodeConfig();
-    }
-
-    private void DetachNodes(AtkUnitBase* addon) {
-        animationContainer?.Dispose();
-        animationContainer = null;
-
-        imageNode?.Dispose();
-        imageNode = null;
     }
 }
---
+++ VanillaPlus/Features/BetterCursor/CursorImageNode.cs
@@ -0,0 +1,81 @@
+﻿using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.System.Input;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Classes.Controllers;
+using KamiToolKit.Classes.Timelines;
+using KamiToolKit.Nodes;
+
+namespace VanillaPlus.Features.BetterCursor;
+
+public unsafe class CursorImageNode : OverlayNode {
+
+    public override OverlayLayer OverlayLayer => OverlayLayer.Foreground;
+
+    public required BetterCursorConfig Config { get; set; }
+
+    private readonly IconImageNode imageNode;
+
+    public CursorImageNode() {
+        imageNode = new IconImageNode {
+            IconId = 60498,
+            FitTexture = true,
+        };
+        imageNode.AttachNode(this);
+        
+        AddTimeline(new TimelineBuilder()
+            .BeginFrameSet(1, 120)
+            .AddLabel(1, 1, AtkTimelineJumpBehavior.Start, 0)
+            .AddLabel(60, 0, AtkTimelineJumpBehavior.LoopForever, 1)
+            .AddLabel(61, 2, AtkTimelineJumpBehavior.Start, 0)
+            .AddLabel(120, 0, AtkTimelineJumpBehavior.LoopForever, 2)
+            .EndFrameSet()
+            .Build());
+
+        imageNode.AddTimeline(new TimelineBuilder()
+            .BeginFrameSet(1, 60)
+            .AddFrame(1, scale: new Vector2(1.0f, 1.0f))
+            .AddFrame(30, scale: new Vector2(0.75f, 0.75f))
+            .AddFrame(60, scale: new Vector2(1.0f, 1.0f))
+            .EndFrameSet()
+            .BeginFrameSet(61, 120)
+            .AddFrame(61, scale: new Vector2(1.0f, 1.0f))
+            .EndFrameSet()
+            .Build());
+    }
+
+    protected override void OnSizeChanged() {
+        base.OnSizeChanged();
+
+        imageNode.Size = Size;
+        imageNode.Origin = new Vector2(Config.Size / 2.0f);
+    }
+
+    public override void Update() {
+        Size = new Vector2(Config.Size);
+
+        imageNode.Color = Config.Color;
+        imageNode.IconId = Config.IconId;
+        
+        Timeline?.PlayAnimation(Config.Animations ? 1 : 2);
+        
+        ref var cursorData = ref UIInputData.Instance()->CursorInputs;
+        Position = new Vector2(cursorData.PositionX, cursorData.PositionY) - imageNode.Size / 2.0f;
+
+        var isLeftHeld = (cursorData.MouseButtonHeldFlags & MouseButtonFlags.LBUTTON) != 0;
+        var isRightHeld = (cursorData.MouseButtonHeldFlags & MouseButtonFlags.RBUTTON) != 0;
+
+        if (Config is { OnlyShowInCombat: true } or { OnlyShowInDuties: true }) {
+            var shouldShow = true;
+            shouldShow &= !Config.OnlyShowInCombat || Services.Condition.IsInCombat();
+            shouldShow &= !Config.OnlyShowInDuties || Services.Condition.IsBoundByDuty();
+            shouldShow &= !Config.HideOnCameraMove || (!isLeftHeld && !isRightHeld);
+
+            IsVisible = shouldShow;
+        }
+        else {
+            IsVisible = !isLeftHeld && !isRightHeld || !Config.HideOnCameraMove;
+        }
+    }
+}
