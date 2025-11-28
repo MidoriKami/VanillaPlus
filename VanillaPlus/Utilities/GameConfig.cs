f3fe126cd95028918034ae995dfaa8b1171e37d8
MidoriKami <mi_kami@outlook.com>
2025-11-28 08:26:45 -0800

    Move audio channel strings into GameConfig helper

---
+++ VanillaPlus/Utilities/GameConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace VanillaPlus.Utilities;
+
+public static class GameConfig {
+    public static readonly string[] AudioConfigOptions = [
+        "IsSndMaster",
+        "IsSndBgm",
+        "IsSndSe",
+        "IsSndVoice",
+        "IsSndEnv",
+        "IsSndSystem",
+        "IsSndPerform",
+    ];
+}
