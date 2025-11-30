82892693ba79a86d88910ec0417d95b58178bfe9
MidoriKami <mi_kami@outlook.com>
2025-11-30 14:47:55 -0800

    Revert "Force default flags and unlock size"
    This reverts commit 7d78892bec458565e02f63d56c1823a40999eff5.

--- VanillaPlus/Features/DebugGameModification/DebugGameModification.cs
+++ VanillaPlus/Features/DebugGameModification/DebugGameModification.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Numerics;
-using Dalamud.Bindings.ImGui;
-using Dalamud.Interface.Windowing;
-using VanillaPlus.Classes;
+﻿using VanillaPlus.Classes;
 
 namespace VanillaPlus.Features.DebugGameModification;
 
@@ -21,27 +17,10 @@ public class DebugGameModification : GameModification {
         ],
     };
 
-    private class DemoWindow() : Window("Exception Test Window", ImGuiWindowFlags.AlwaysAutoResize) {
-        public override void Draw() {
-            ImGui.Text("butts");
-            ImGui.PushStyleColor(ImGuiCol.WindowBg, Vector4.Zero);
-            throw new Exception("Boom");
-            ImGui.Text("Butts2");
-        }
-    }
-    
-    private WindowSystem windowSystem = new WindowSystem("Debug Game Modification");
-    private DemoWindow window = new DemoWindow();
-
     public override void OnEnable() {
-        windowSystem.AddWindow(window);
-        window.Open();
-        
-        Services.PluginInterface.UiBuilder.Draw += windowSystem.Draw;
     }
 
     public override void OnDisable() {
-        Services.PluginInterface.UiBuilder.Draw -= windowSystem.Draw;
     }
 }
 #endif
