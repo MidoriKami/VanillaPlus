5e6c20d38a513378d14ca221b78309e00649a938
MidoriKami <mi_kami@outlook.com>
2025-12-19 09:08:31 -0800

    Simplify addon logger

--- VanillaPlus/Extensions/AddonLifecycleExtensions.cs
+++ VanillaPlus/Extensions/AddonLifecycleExtensions.cs
@@ -1,39 +1,40 @@
-﻿using Dalamud.Game.Addon.Lifecycle;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace VanillaPlus.Extensions;
 
-public record LoggedAddonEvents {
-    public bool Setup { get; set; }
-    public bool Refresh { get; set; }
-    public bool ReceiveEvent { get; set; }
-    public bool RequestedUpdate { get; set; }
-    public bool Update { get; set; }
-    public bool Draw { get; set; }
-    public bool Finalize { get; set; }
-}
-
 public static class AddonLifecycleExtensions {
-    public static void LogAddon(this IAddonLifecycle addonLifecycle, string addonName, LoggedAddonEvents? loggedModules = null) {
-        if (loggedModules is not null) {
-            if (loggedModules.Setup) addonLifecycle.RegisterListener(AddonEvent.PostSetup, addonName, Logger);
-            if (loggedModules.Refresh) addonLifecycle.RegisterListener(AddonEvent.PostRefresh, addonName, Logger);
-            if (loggedModules.ReceiveEvent) addonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, addonName, Logger);
-            if (loggedModules.RequestedUpdate) addonLifecycle.RegisterListener(AddonEvent.PostRequestedUpdate, addonName, Logger);
-            if (loggedModules.Update) addonLifecycle.RegisterListener(AddonEvent.PostUpdate, addonName, Logger);
-            if (loggedModules.Draw) addonLifecycle.RegisterListener(AddonEvent.PostDraw, addonName, Logger);
-            if (loggedModules.Finalize) addonLifecycle.RegisterListener(AddonEvent.PreFinalize, addonName, Logger);
+    extension(IAddonLifecycle addonLifecycle) {
+        public void LogAddon(string addonName, params AddonEvent[] loggedModules) {
+            if (loggedModules.Length is 0) {
+                loggedModules = [
+                    AddonEvent.PostSetup, AddonEvent.PostOpen,
+                    AddonEvent.PostClose, AddonEvent.PostShow, 
+                    AddonEvent.PostHide, AddonEvent.PostRefresh,
+                    AddonEvent.PostRequestedUpdate, AddonEvent.PreFinalize,
+                ];
+            }
+
+            ActiveLoggers.TryAdd(addonName, loggedModules.ToList());
+            foreach (var loggedModule in loggedModules) {
+                addonLifecycle.RegisterListener(loggedModule, addonName, Logger);
+            }
         }
-        else {
-            addonLifecycle.RegisterListener(AddonEvent.PostSetup, addonName, Logger);
-            addonLifecycle.RegisterListener(AddonEvent.PostRefresh, addonName, Logger);
-            addonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, addonName, Logger);
-            addonLifecycle.RegisterListener(AddonEvent.PostRequestedUpdate, addonName, Logger);
-            addonLifecycle.RegisterListener(AddonEvent.PreFinalize, addonName, Logger);
+
+        public void UnLogAddon(string addonName) {
+            if (!ActiveLoggers.TryGetValue(addonName, out var loggedModules)) return;
+
+            foreach (var loggedModule in loggedModules) {
+                addonLifecycle.UnregisterListener(loggedModule, addonName, Logger);
+            }
         }
     }
+
+    private static readonly Dictionary<string, List<AddonEvent>> ActiveLoggers = [];
 
     private static void Logger(AddonEvent type, AddonArgs args) {
         switch (args) {
@@ -45,15 +46,5 @@ public static class AddonLifecycleExtensions {
                 Services.PluginLog.Debug($"{args.AddonName} called {type.ToString().Replace("Post", string.Empty)}");
                 break;
         }
-    }
-
-    public static void UnLogAddon(this IAddonLifecycle addonLifecycle, string addonName) {
-        addonLifecycle.UnregisterListener(AddonEvent.PostSetup, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PostRefresh, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PostReceiveEvent, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PostRequestedUpdate, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PreFinalize, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PostDraw, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PostUpdate, addonName, Logger);
     }
 }
