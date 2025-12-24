5ba42081d58dd8bd7c8971323826811b1c3644e4
Zeffuro <Jeffroiscool@gmail.com>
2025-12-24 04:16:23 +0100

    Add Image and comment

--- VanillaPlus/Extensions/ActionManagerExtensions.cs
+++ VanillaPlus/Extensions/ActionManagerExtensions.cs
@@ -5,9 +5,12 @@ namespace VanillaPlus.Extensions;
 
 public static unsafe class ActionManagerExtensions {
     extension(ref ActionManager actionManager) {
-        public float GetRecastTimeLeft(ActionType actionType, uint actionId) 
+        public float GetRecastTimeLeft(ActionType actionType, uint actionId)
             => actionManager.GetRecastTime(actionType, actionId) - actionManager.GetRecastTimeElapsed(actionType, actionId);
 
+        /// <summary>
+        /// Gets the RecastDetails for the specified action, CooldownGroup 58 is seemingly used for actions that both have their own cooldown in addition to adhering to GCD.
+        /// </summary>
         public RecastDetail* GetRecastDetail(Action action) {
             var group = action.CooldownGroup == 58 ? action.AdditionalCooldownGroup : actionManager.GetRecastGroup(1, action.RowId);
             return actionManager.GetRecastGroupDetail(group);
