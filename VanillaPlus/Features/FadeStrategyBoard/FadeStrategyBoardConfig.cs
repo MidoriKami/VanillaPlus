11ef5fa79f11ff418b4bee7df64abbc6531d1456
MidoriKami <mi_kami@outlook.com>
2025-12-25 21:05:28 -0800

    Add FadeStrategyBoard

---
+++ VanillaPlus/Features/FadeStrategyBoard/FadeStrategyBoardConfig.cs
@@ -0,0 +1,9 @@
+﻿using VanillaPlus.Classes;
+
+namespace VanillaPlus.Features.FadeStrategyBoard;
+
+public class FadeStrategyBoardConfig : GameModificationConfig<FadeStrategyBoardConfig> {
+    protected override string FileName => "FadeStrategyBoard";
+
+    public float FadePercentage = 0.8f;
+}
