73013e9760f69501f2df2988df04d3ff951d07ca
GrittyFrog <grittyfrog@fastmail.com>
2025-12-31 11:14:06 +1100

    DutyLootPreview: Update Changelog

--- VanillaPlus/Features/DutyLootPreview/DutyLootPreview.cs
+++ VanillaPlus/Features/DutyLootPreview/DutyLootPreview.cs
@@ -13,7 +13,8 @@ public class DutyLootPreview : GameModification {
             new ChangeLogInfo(1, "Initial Implementation"),
             new ChangeLogInfo(2, "Added Context Menu, Filter Buttons, Favorites and\nItem Drop info"),
             new ChangeLogInfo(3, "Added button to view current duty loot"),
-            new ChangeLogInfo(4, "- Loot is now properly loaded when opening the\n  Duty Loot Preview from the Duty Journal\n- Unlocked items now show a checkmark")
+            new ChangeLogInfo(4, "- Loot is now properly loaded when opening the\n  Duty Loot Preview from the Duty Journal\n- Unlocked items now show a checkmark"),
+            new ChangeLogInfo(5, "- Arm Containers now appear under Equipment\n- In Duty button no longer appears above all windows")
         ],
     };
 
