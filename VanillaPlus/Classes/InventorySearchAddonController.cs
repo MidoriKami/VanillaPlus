1f54cd522b6d973c940cdc946ab27a4a994166d1
MidoriKami <mi_kami@outlook.com>
2025-12-31 16:42:36 -0800

    Use named functions for troubleshooting

--- VanillaPlus/Classes/InventorySearchAddonController.cs
+++ VanillaPlus/Classes/InventorySearchAddonController.cs
@@ -18,44 +18,12 @@ public unsafe class InventorySearchAddonController : IDisposable {
         selectedTabs = [];
 
         inventoryController = new MultiAddonController(addons);
-
-        inventoryController.OnAttach += addon => {
-            var size = new Vector2(addon->Size.X / 2.0f, 28.0f);
-
-            var headerSize = new Vector2(addon->WindowHeaderCollisionNode->Width, addon->WindowHeaderCollisionNode->Height);
-            var newInputNode = new TextInputWithHintNode {
-                Position = headerSize / 2.0f - size / 2.0f + new Vector2(25.0f, 10.0f),
-                Size = size,
-                OnInputReceived = searchString => PerformSearch(addon, searchString.ToString()),
-            };
-
-            newInputNode.AttachNode(addon);
-            inputTextNodes.TryAdd(addon->NameString, newInputNode);
-        };
-
-        inventoryController.OnUpdate += addon => {
-            if (!addon->IsReady) return;
-
-            var currentTab = InventorySearchController.GetTabForInventory(addon);
-            
-            selectedTabs.TryAdd(addon->NameString, currentTab);
-            if (selectedTabs[addon->NameString] != currentTab && inputTextNodes.TryGetValue(addon->NameString, out var inputTextNode)) {
-                PerformSearch(addon, inputTextNode.SearchString.ToString());
-            }
-
-            selectedTabs[addon->NameString] = currentTab;
-        };
-
-        inventoryController.OnDetach += addon => {
-            if (inputTextNodes.TryGetValue(addon->NameString, out var node)) {
-                node.Dispose();
-                inputTextNodes.Remove(addon->NameString);
-            }
-        };
-
+        inventoryController.OnAttach += OnInventoryAttach;
+        inventoryController.OnUpdate += OnInventoryUpdate;
+        inventoryController.OnDetach += OnInventoryDetach;
         inventoryController.Enable();
     }
-    
+
     public void Dispose() {
         inventoryController?.Dispose();
         inventoryController = null;
@@ -69,6 +37,41 @@ public unsafe class InventorySearchAddonController : IDisposable {
                 
         selectedTabs?.Clear();
         selectedTabs = null;
+    }
+    
+    private void OnInventoryDetach(AtkUnitBase* addon) {
+        if (inputTextNodes?.TryGetValue(addon->NameString, out var node) ?? false) {
+            node.Dispose();
+            inputTextNodes.Remove(addon->NameString);
+        }
+    }
+
+    private void OnInventoryUpdate(AtkUnitBase* addon) {
+        if (selectedTabs is null) return;
+        if (inputTextNodes is null) return;
+        if (!addon->IsReady) return;
+
+        var currentTab = InventorySearchController.GetTabForInventory(addon);
+
+        selectedTabs.TryAdd(addon->NameString, currentTab);
+        if (selectedTabs[addon->NameString] != currentTab && inputTextNodes.TryGetValue(addon->NameString, out var inputTextNode)) {
+            PerformSearch(addon, inputTextNode.SearchString.ToString());
+        }
+
+        selectedTabs[addon->NameString] = currentTab;
+    }
+
+    private void OnInventoryAttach(AtkUnitBase* addon) {
+        if (inputTextNodes is null) return;
+        var size = new Vector2(addon->Size.X / 2.0f, 28.0f);
+
+        var headerSize = new Vector2(addon->WindowHeaderCollisionNode->Width, addon->WindowHeaderCollisionNode->Height);
+        var newInputNode = new TextInputWithHintNode {
+            Position = headerSize / 2.0f - size / 2.0f + new Vector2(25.0f, 10.0f), Size = size, OnInputReceived = searchString => PerformSearch(addon, searchString.ToString()),
+        };
+
+        newInputNode.AttachNode(addon);
+        inputTextNodes.TryAdd(addon->NameString, newInputNode);
     }
 
     public Action<string>? PreSearch { get; set; }
