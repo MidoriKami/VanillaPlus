a0a3889e4fce95eaefb287ad98e2dd3126ace9f9
MidoriKami <mi_kami@outlook.com>
2026-01-03 11:29:36 -0800

    Fix changelog word wrap

--- VanillaPlus/InternalSystem/AddonChangelogBrowser.cs
+++ VanillaPlus/InternalSystem/AddonChangelogBrowser.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Linq;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit;
 using KamiToolKit.Classes;
@@ -11,51 +9,42 @@ namespace VanillaPlus.InternalSystem;
 
 public class AddonChangelogBrowser : NativeAddon {
 
-    private ScrollingAreaNode<TreeListNode>? scrollingAreaNode;
-    
-    private readonly List<TreeListCategoryNode> categoryNodes = [];
+    private ScrollingTreeNode? scrollingTreeNode;
 
     protected override unsafe void OnSetup(AtkUnitBase* addon) {
-        scrollingAreaNode = new ScrollingAreaNode<TreeListNode> {
+        scrollingTreeNode = new ScrollingTreeNode {
             Size = ContentSize,
             Position = ContentStartPosition,
-            ContentHeight = 1000.0f,
             ScrollSpeed = 100,
             AutoHideScrollBar = true,
         };
-        scrollingAreaNode.AttachNode(this);
+        scrollingTreeNode.AttachNode(this);
 
         if (Modification is not null) {
-            categoryNodes.Clear();
-
             foreach (var changelog in Modification.ModificationInfo.ChangeLog.OrderByDescending(log => log.Version)) {
                 var categoryNode = new TreeListCategoryNode {
-                    SeString = Strings.VersionLabelFormat.Format(changelog.Version),
-                    Width = ContentSize.X,
-                    OnToggle = _ => scrollingAreaNode.ContentHeight = categoryNodes.Sum(node => node.Height),
+                    String = Strings.VersionLabelFormat.Format(changelog.Version),
+                    OnToggle = _ => scrollingTreeNode.RecalculateLayout(),
                 };
 
                 var newTextNode = new TextNode {
-                    Height = 32.0f,
-                    AlignmentType = AlignmentType.TopLeft,
-                    TextFlags = TextFlags.MultiLine | TextFlags.WordWrap | TextFlags.AutoAdjustNodeSize,
-                    Position = new Vector2(6.0f, 2.0f),
-                    Width = ContentSize.X - 32.0f,
+                    Width = scrollingTreeNode.TreeListNode.Width,
+                    TextFlags = TextFlags.MultiLine | TextFlags.WordWrap,
                     FontSize = 14,
                     LineSpacing = 22,
-                    String = changelog.Description,
                     TextColor = ColorHelper.GetColor(1),
                 };
 
-                newTextNode.Height = newTextNode.GetTextDrawSize(newTextNode.SeString).Y;
+                newTextNode.String = changelog.Description;
+                newTextNode.Height = newTextNode.GetTextDrawSize(newTextNode.String).Y;
+                
+                categoryNode.RecalculateLayout();
                 
                 categoryNode.AddNode(newTextNode);
-
-                scrollingAreaNode.ContentNode.AddCategoryNode(categoryNode);
-                categoryNodes.Add(categoryNode);
+                scrollingTreeNode.AddCategoryNode(categoryNode);
             }
 
-            scrollingAreaNode.ContentHeight = categoryNodes.Sum(node => node.Height);
+            scrollingTreeNode.RecalculateLayout();
         }
     }
 
