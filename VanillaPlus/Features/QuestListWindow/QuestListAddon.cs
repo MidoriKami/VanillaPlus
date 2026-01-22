6526c301d13556452ac76fa415dd0f4b9581a5c5
MidoriKami <mi_kami@outlook.com>
2026-01-21 18:47:00 -0800

    Remove performance optimizatins from QuestListAddon

--- VanillaPlus/Features/QuestListWindow/QuestListAddon.cs
+++ VanillaPlus/Features/QuestListWindow/QuestListAddon.cs
@@ -8,7 +8,6 @@ using Map = FFXIVClientStructs.FFXIV.Client.Game.UI.Map;
 namespace VanillaPlus.Features.QuestListWindow;
 
 public unsafe class QuestListAddon : SearchableNodeListAddon<MarkerInfo, QuestListItemNode> {
-    private int? lastQuestCount;
     private QuestFilterMode lastSortingMode = QuestFilterMode.Type;
     private bool isReversed;
 
@@ -18,21 +17,10 @@ public unsafe class QuestListAddon : SearchableNodeListAddon<MarkerInfo, QuestLi
     }
 
     protected override void OnUpdate(AtkUnitBase* addon) {
-        if (lastQuestCount != Map.Instance()->UnacceptedQuestMarkers.Count) {
-            ListNode?.OptionsList = Map.Instance()->UnacceptedQuestMarkers.ToList();
-
-            lastQuestCount = Map.Instance()->UnacceptedQuestMarkers.Count;
-        }
-
+        ListNode?.OptionsList = Map.Instance()->UnacceptedQuestMarkers.ToList();
         ListItems.Sort((left, right) => MarkerInfoExtensions.Comparison(left, right, lastSortingMode) * (isReversed ? -1 : 1));
-        
+
         base.OnUpdate(addon);
-    }
-
-    protected override void OnFinalize(AtkUnitBase* addon) {
-        base.OnFinalize(addon);
-
-        lastQuestCount = null;
     }
 
     private void UpdateSorting(string newFilterString, bool reversed) {
