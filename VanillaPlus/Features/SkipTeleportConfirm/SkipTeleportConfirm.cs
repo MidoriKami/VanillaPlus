81d109a7db66053b40dce17defe660ce9f6ad3d7
MidoriKami <mi_kami@outlook.com>
2026-03-12 16:07:01 -0700

    Split SelectYesNoHandler into named function

--- VanillaPlus/Features/SkipTeleportConfirm/SkipTeleportConfirm.cs
+++ VanillaPlus/Features/SkipTeleportConfirm/SkipTeleportConfirm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Game.Agent;
 using Dalamud.Game.Agent.AgentArgTypes;
 using FFXIVClientStructs.FFXIV.Client.UI;
@@ -35,18 +36,20 @@ public unsafe class SkipTeleportConfirm : GameModification {
         var valueSpan = new Span<AtkValue>((AtkValue*)receiveEventArgs.AtkValues, valueCount);
 
         if (receiveEventArgs.ValueCount is 2 && valueSpan[0].Int is 7) {
-            Services.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "SelectYesno", (_, yesNoArgs) => {
-                var addon = yesNoArgs.GetAddon<AddonSelectYesno>();
-
-                if (addon->AtkUnitBase.GetCallbackHandlerInfo() is { AgentId: CsAgentId.Map, EventKind: 1 }) {
-                    var newValues = stackalloc AtkValue[1];
-                    newValues->SetInt(0);
-
-                    addon->FireCallback(1, newValues, true);
-                }
-
-                Services.AddonLifecycle.UnregisterListener(AddonEvent.PostSetup, "SelectYesno");
-            });
+            Services.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "SelectYesno", SelectYesNoHandler);
         }
+    }
+
+    private static void SelectYesNoHandler(AddonEvent _, AddonArgs yesNoArgs) {
+        var addon = yesNoArgs.GetAddon<AddonSelectYesno>();
+
+        if (addon->AtkUnitBase.GetCallbackHandlerInfo() is { AgentId: CsAgentId.Map, EventKind: 1 }) {
+            var newValues = stackalloc AtkValue[1];
+            newValues->SetInt(0);
+
+            addon->FireCallback(1, newValues, true);
+        }
+
+        Services.AddonLifecycle.UnregisterListener(SelectYesNoHandler);
     }
 }
