2cd4561e423451648d870ca1aaa15e50f484c74b
MidoriKami <mi_kami@outlook.com>
2026-03-17 12:08:22 -0700

    Add Show Aetherytes on Top

---
+++ VanillaPlus/Classes/QuestAwayCompatabilityModule.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace VanillaPlus.Classes;
+
+public class QuestAwayCompatabilityModule : CompatibilityModule {
+
+    public override bool ShouldLoadGameModification() {
+        // If SimpleTweaks is not loaded, we can load our module
+        if (!IsQuestAwayLoaded()) return true;
+        
+        // If SimpleTweaks is loaded, but doesn't contain our module, then we can load our module
+        return !IsAetheryteFeatureEnabled();
+    }
+
+    public override string GetErrorMessage()
+        => "Not compatible with QuestAWAYS's 'Aetherytes always in front' feature";
+    
+    private static bool IsAetheryteFeatureEnabled() {
+        var configFileInfo = GetConfigFileInfo();
+        if (configFileInfo.Exists) {
+            var fileText = File.ReadAllText(configFileInfo.FullName);
+
+            if (fileText.IsNullOrEmpty()) return false;
+            
+            var jObject = JObject.Parse(fileText);
+            if (!jObject.HasValues) return false;
+
+            var aetheryteInFront = jObject.GetValue("AetheryteInFront");
+            if (aetheryteInFront is null) return false;
+
+            if (aetheryteInFront.Type is JTokenType.Boolean) {
+                return aetheryteInFront.ToObject<bool>();
+            }
+        }
+
+        return false;
+    }
+    
+    private static string GetConfigFilePath()
+        => Path.Combine(Services.PluginInterface.GetPluginConfigDirectory().Replace("VanillaPlus", "QuestAWAY.json"));
+    
+    private static FileInfo GetConfigFileInfo()
+        => new(GetConfigFilePath());
+    
+    private bool IsQuestAwayLoaded()
+        => IsPluginLoaded("QuestAWAY");
+}
---
+++ VanillaPlus/Features/ShowAetherytesOnTop/ShowAetherytesOnTop.cs
@@ -0,0 +1,53 @@
+﻿using Dalamud.Game.Agent;
+using Dalamud.Game.Agent.AgentArgTypes;
+using FFXIVClientStructs.FFXIV.Client.System.Input;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using VanillaPlus.Classes;
+using VanillaPlus.Enums;
+using AgentId = Dalamud.Game.Agent.AgentId;
+
+namespace VanillaPlus.Features.ShowAetherytesOnTop;
+
+public unsafe class ShowAetherytesOnTop : GameModification {
+    public override ModificationInfo ModificationInfo => new() {
+        DisplayName = "Show Aetherytes on Top",
+        Description = "Forces aetherytes to always show on top of other map markers.",
+        Type = ModificationType.UserInterface,
+        SubType = ModificationSubType.Map,
+        Authors = [ "MidoriKami" ],
+        ChangeLog = [
+            new ChangeLogInfo(1, "Initial Implementation"),
+        ],
+        CompatibilityModule = new QuestAwayCompatabilityModule(),
+    };
+
+    private KeyStateFlags controlPreState;
+
+    public override void OnEnable() {
+        Services.AgentLifecycle.RegisterListener(AgentEvent.PreUpdate, AgentId.Map, OnMapPreUpdate);
+        Services.AgentLifecycle.RegisterListener(AgentEvent.PostUpdate, AgentId.Map, OnMapPostUpdate);
+    }
+
+    public override void OnDisable() {
+        Services.AgentLifecycle.UnregisterListener(OnMapPreUpdate, OnMapPostUpdate);
+        AgentMap.Instance()->IsControlKeyPressed = true;
+    }
+
+    private void OnMapPreUpdate(AgentEvent type, AgentArgs args) {
+        controlPreState = ControlKeyState;
+        ControlKeyState = KeyStateFlags.Down | KeyStateFlags.Held;
+    }
+
+    private void OnMapPostUpdate(AgentEvent type, AgentArgs args) {
+        if (!controlPreState.HasFlag(KeyStateFlags.Down) && !controlPreState.HasFlag(KeyStateFlags.Held)) {
+            ControlKeyState = KeyStateFlags.None;
+            args.GetAgentPointer<AgentMap>()->IsControlKeyPressed = false;
+        }
+    }
+
+    private static KeyStateFlags ControlKeyState {
+        get => UIModule.Instance()->GetUIInputData()->KeyboardInputs.KeyState[(int)SeVirtualKey.CONTROL];
+        set => UIModule.Instance()->GetUIInputData()->KeyboardInputs.KeyState[(int)SeVirtualKey.CONTROL] = value;
+    }
+}
