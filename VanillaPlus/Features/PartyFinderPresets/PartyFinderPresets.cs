a44da2fd4dbcf13dfffa973d30c505e0187da13b
MidoriKami <mi_kami@outlook.com>
2026-03-20 19:03:37 -0700

    Fix buttons showing when they shouldn't

--- VanillaPlus/Features/PartyFinderPresets/PartyFinderPresets.cs
+++ VanillaPlus/Features/PartyFinderPresets/PartyFinderPresets.cs
@@ -47,7 +47,6 @@ public unsafe class PartyFinderPresets : GameModification {
             Title = Strings.Title_PresetConfigManager,
             Options = GetPresetInfos(),
             ItemSpacing = 2.0f,
-            EnableAddButton = false,
             RemoveClicked = (_, toRemove) => {
                 PresetManager.DeletePreset(toRemove);
                 UpdateDropDownOptions();
