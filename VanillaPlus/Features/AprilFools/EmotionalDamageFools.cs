7d4093731131370719a7a0c2bf49a291876c42bb
MidoriKami <mi_kami@outlook.com>
2026-03-28 12:11:43 -0700

    Increase intensity of emotional damage

--- VanillaPlus/Features/AprilFools/EmotionalDamageFools.cs
+++ VanillaPlus/Features/AprilFools/EmotionalDamageFools.cs
@@ -14,7 +14,7 @@ public class EmotionalDamageFools : IFoolsModule {
 
     private void OnFlyText(ref FlyTextKind kind, ref int val1, ref int val2, ref SeString text1, ref SeString text2, ref uint color, ref uint icon, ref uint damageTypeIcon, ref float yOffset, ref bool handled) {
         if (!Config.EmotionalDamage) return;
-        if (kind is not FlyTextKind.Miss) return;
+        if (kind is FlyTextKind.Damage) return;
 
         Services.FlyTextGui.AddFlyText(
             FlyTextKind.Damage, 
