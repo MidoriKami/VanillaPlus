23630a44e0d3e05d683e50c81cdd0fd976fd033d
MidoriKami <mi_kami@outlook.com>
2026-03-28 11:33:16 -0700

    Add Seasonal Event Notice

--- VanillaPlus/PluginSystem.cs
+++ VanillaPlus/PluginSystem.cs
@@ -1,11 +1,13 @@
 using VanillaPlus.Classes;
 using VanillaPlus.InternalSystem;
+using VanillaPlus.NativeElements.Addons;
 
 namespace VanillaPlus;
 
 public static class PluginSystem {
     public static SystemConfiguration SystemConfig { get; set; } = null!;
     public static AddonModificationBrowser AddonModificationBrowser { get; set; } = null!;
+    public static SeasonEventAddon SeasonEventAddon { get; set; } = null!;
     public static ModificationManager ModificationManager { get; set; } = null!;
     public static KeyListener KeyListener { get; set; } = null!;
 }
