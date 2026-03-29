34b9b48e9fa48d8f82fb79227ce2b7d38d6521a6
MidoriKami <mi_kami@outlook.com>
2026-03-29 14:53:59 -0700

    Add LabelEntry to Config Entries

---
+++ VanillaPlus/NativeElements/Config/ConfigEntries/LabelEntry.cs
@@ -0,0 +1,16 @@
+﻿using KamiToolKit;
+using KamiToolKit.Nodes;
+
+namespace VanillaPlus.NativeElements.Config.ConfigEntries;
+
+public class LabelEntry : IConfigEntry {
+    public string? Tooltip { get; set; }
+    public required string Text { get; set; }
+    
+    public void Dispose() {
+    }
+
+    public NodeBase BuildNode() => new TextNode {
+        String = Text,
+    };
+}
