d97a27d1e95ab24a6addf4dfe0c5890ec565903b
bilk <cR0N7CXAC9O0yJ9dd@gmail.com>
2026-04-01 22:05:47 +0100

    cleanup

--- VanillaPlus/Features/SuppressSharedBoards/SuppressSharedBoards.cs
+++ VanillaPlus/Features/SuppressSharedBoards/SuppressSharedBoards.cs
@@ -16,30 +16,28 @@ public class SuppressSharedBoards : GameModification {
         ],
     };
 
-    private delegate void ReceiveSharedPopupDelegate(ulong a1, byte a2, byte a3);
+    private delegate void ReceiveSharedPopupDelegate(nint thisPtr, byte a2, bool a3);
     [Signature("48 89 6C 24 ?? 56 41 56 41 57 48 83 EC ?? 4C 8B F9 0F B6 EA", DetourName = nameof(ReceiveSharedPopupDetour))]
-    private Hook<ReceiveSharedPopupDelegate> receiveSharedPopupHook = null!;
+    private Hook<ReceiveSharedPopupDelegate>? receiveSharedPopupHook;
 
-    private delegate void ReceiveSharedSaveDelegate(ulong a1, ulong a2, ulong a3, int a4, uint a5);
+    private delegate void ReceiveSharedSaveDelegate(nint thisPtr, nint a2, nint a3, int a4, uint a5);
     [Signature("E8 ?? ?? ?? ?? 40 80 F5", DetourName = nameof(ReceiveSharedSaveDetour))]
-    private Hook<ReceiveSharedSaveDelegate> receiveSharedSaveHook = null!;
+    private Hook<ReceiveSharedSaveDelegate>? receiveSharedSaveHook;
 
     public override void OnEnable() {
         Services.GameInteropProvider.InitializeFromAttributes(this);
-        receiveSharedPopupHook.Enable();
-        receiveSharedSaveHook.Enable();
+        receiveSharedPopupHook?.Enable();
+        receiveSharedSaveHook?.Enable();
     }
 
     public override void OnDisable() {
-        receiveSharedPopupHook.Dispose();
-        receiveSharedSaveHook.Dispose();
+        receiveSharedPopupHook?.Dispose();
+        receiveSharedPopupHook = null;
+        receiveSharedSaveHook?.Dispose();
+        receiveSharedSaveHook = null;
     }
 
-    private void ReceiveSharedPopupDetour(ulong a1, byte a2, byte a3) {
-        return;
-    }
+    private void ReceiveSharedPopupDetour(nint thisPtr, byte a2, bool a3) { }
 
-    private void ReceiveSharedSaveDetour(ulong a1, ulong a2, ulong a3, int a4, uint a5) {
-        return;
-    }
+    private void ReceiveSharedSaveDetour(nint thisPtr, nint a2, nint a3, int a4, uint a5) { }
 }
