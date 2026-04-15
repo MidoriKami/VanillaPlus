01ebeb1ff073d3bfa53104da32eea8967dcc83dd
Zack Campbell <zajrik@users.noreply.github.com>
2026-04-15 09:26:49 -0500

    fix feature folder

---
+++ VanillaPlus/Features/GearSetReorderButtons/GearSetListUiController.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Controllers;
+using VanillaPlus.Features.GearSetReorderButtons.Nodes;
+
+namespace VanillaPlus.Features.GearSetReorderButtons;
+
+public unsafe class GearSetListUiController {
+    private AddonController<AddonGearSetList>? gearSetsAddonController;
+    private NativeListController<AddonGearSetList, GearSetListListItem>? gearSetsListController;
+
+    private readonly Dictionary<uint, GearSetListReorderButtonNode> reorderButtonNodes = [];
+
+    private const ushort ExtraAddonWidth = 58;
+
+    public void OnEnable() {
+        gearSetsAddonController = new() {
+            AddonName = "GearSetList",
+            OnSetup = SetUpAddon,
+            OnFinalize = FinalizeAddon,
+        };
+        gearSetsAddonController.Enable();
+
+        gearSetsListController = new() {
+            AddonName = "GearSetList",
+            GetPopulatorNode = GetPopulatorNode,
+            ShouldModifyElement = ShouldModifyElement,
+            UpdateElement = UpdateElement,
+            ResetElement = ResetElement
+        };
+        gearSetsListController.Enable();
+    }
+
+    public void OnDisable() {
+        gearSetsAddonController?.Dispose();
+        gearSetsListController?.Dispose();
+
+        gearSetsAddonController = null;
+        gearSetsListController = null;
+    }
+
+    private void SetUpAddon(AddonGearSetList* addon) {
+        // Tried to use AtkUnitBase.Resize() but it doesn't update the position/width
+        // of all of the nodes needed to make room for the reorder buttons so I'm still
+        // doing this manually. It's still gross and I still hate it.
+
+        // Get all the nodes that need widened
+        var nodesToWiden = new AtkResNode*[] {
+            addon->GetNodeById(11),
+            addon->GetNodeById(8),
+            addon->GetNodeById(7),
+            addon->GetNodeById(5),
+
+            addon->GetComponentByNodeId(11)->GetNodeById(2),
+
+            (AtkResNode*)addon->GetComponentByNodeId(11)->GetCollisionNodeById(12),
+            (AtkResNode*)addon->GetComponentByNodeId(11)->GetCollisionNodeById(11),
+            (AtkResNode*)addon->GetComponentByNodeId(11)->GetNineGridNodeById(10),
+            (AtkResNode*)addon->GetComponentByNodeId(11)->GetNineGridNodeById(9),
+            (AtkResNode*)addon->GetComponentByNodeId(11)->GetImageNodeById(8),
+
+            (AtkResNode*)addon->GetComponentByNodeId(7)->GetCollisionNodeById(6),
+        };
+
+        foreach (var node in nodesToWiden) {
+            if (node is null) continue;
+            node->Width += ExtraAddonWidth;
+        }
+
+        // Get all the nodes that need repositioned
+        var nodesToReposition = new AtkResNode*[] {
+            addon->GetNodeById(10),
+            addon->GetNodeById(2),
+            addon->GetComponentByNodeId(11)->GetNodeById(7),
+            addon->GetComponentByNodeId(7)->GetNodeById(4),
+        };
+
+        foreach (var node in nodesToReposition) {
+            if (node is null) continue;
+            node->X += ExtraAddonWidth;
+        }
+    }
+
+    private void FinalizeAddon(AddonGearSetList* addon) {
+        // Dispose of all reorder button nodes
+        reorderButtonNodes.Values.ToList().ForEach(it => it.Dispose());
+        reorderButtonNodes.Clear();
+    }
+
+    private AtkComponentListItemRenderer* GetPopulatorNode(AddonGearSetList* addon) {
+        return addon->GetComponentListById(7)->FirstAtkComponentListItemRenderer;
+    }
+
+    private bool ShouldModifyElement(AddonGearSetList* addon, GearSetListListItem listItemData) {
+        return true;
+    }
+
+    private void UpdateElement(AddonGearSetList* addon, GearSetListListItem listItemData) {
+        // Update reorder button node if it already exists
+        if (reorderButtonNodes.TryGetValue(listItemData.NodeId, out var reorderButton)) {
+            reorderButton.Update(listItemData);
+            return;
+        }
+
+        // Otherwise create and cache new reorder button node
+        var anchorNode = listItemData.ButtonAnchorNode;
+
+        reorderButton = new GearSetListReorderButtonNode() {
+            Position = new Vector2(anchorNode->X + anchorNode->Width, 0)
+        };
+
+        reorderButtonNodes.Add(listItemData.NodeId, reorderButton);
+        reorderButton.Update(listItemData);
+        reorderButton.AttachNode(anchorNode);
+    }
+
+    private void ResetElement(AddonGearSetList* addon, GearSetListListItem listItemData) { }
+
+}
---
+++ VanillaPlus/Features/GearSetReorderButtons/Nodes/GearSetListReorderButtonNode.cs
@@ -0,0 +1,52 @@
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using KamiToolKit.Nodes;
+using KamiToolKit.Premade.Node.Simple;
+
+namespace VanillaPlus.Features.GearSetReorderButtons.Nodes;
+
+public unsafe class GearSetListReorderButtonNode : SimpleComponentNode {
+    public int GearSetId { get; set; }
+
+    private readonly CircleButtonNode upButtonNode;
+    private readonly CircleButtonNode downButtonNode;
+
+    private byte GearSetCount => RaptureGearsetModule.Instance()->NumGearsets;
+
+    public GearSetListReorderButtonNode() {
+        upButtonNode = new CircleButtonNode {
+            Icon = ButtonIcon.UpArrow,
+            Size = new Vector2(32.0f, 32.0f),
+            OnClick = () => AgentGearSet.Instance()->MoveSetUp(GearSetId),
+            IsEnabled = false
+        };
+
+        downButtonNode = new CircleButtonNode {
+            Icon = ButtonIcon.ArrowDown,
+            Size = new Vector2(32.0f, 32.0f),
+            Position = new Vector2(28.0f, 0.0f),
+            OnClick = () => AgentGearSet.Instance()->MoveSetDown(GearSetId),
+            IsEnabled = false
+        };
+
+        upButtonNode.AttachNode(this);
+        downButtonNode.AttachNode(upButtonNode);
+    }
+
+    public void Update(GearSetListListItem listItemData) {
+        GearSetId = listItemData.GearSetId;
+
+        upButtonNode.IsEnabled = listItemData.GearSetIndex > 0;
+        downButtonNode.IsEnabled = listItemData.GearSetIndex < GearSetCount - 1;
+
+        IsVisible = listItemData.IsChecked;
+    }
+
+    protected override void Dispose(bool disposing, bool isNativeDestructor) {
+        downButtonNode.Dispose();
+        upButtonNode.Dispose();
+
+        base.Dispose(disposing, isNativeDestructor);
+    }
+}
