2527a2a790dd63a8eff8a2d18bf819b28c866895
Treezy <cR0N7CXAC9O0yJ9dd@gmail.com>
2026-04-24 16:14:42 +0100

    Add Suppress Shared Strategy Boards Tweak (#185)
    Add Suppress Shared Strategy Boards

---
+++ VanillaPlus/Features/SuppressSharedBoards/SuppressSharedBoards.cs
@@ -0,0 +1,43 @@
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using VanillaPlus.Classes;
+using VanillaPlus.Enums;
+
+namespace VanillaPlus.Features.SuppressSharedBoards;
+
+public class SuppressSharedBoards : GameModification {
+    public override ModificationInfo ModificationInfo => new() {
+        DisplayName = "Suppress Shared Strategy Boards",
+        Description = "Completely suppresses any shared Strategy Board.",
+        Type = ModificationType.GameBehavior,
+        Authors = ["Treezy"]
+    };
+
+    // TODO: Replace with CS in 7.5
+    // https://github.com/aers/FFXIVClientStructs/blob/main/FFXIVClientStructs/FFXIV/Client/UI/Misc/TofuHelper.cs#L58
+    private delegate void ShowSharedNotificationDelegate(nint thisPtr, bool isNotRealTimeSharing, bool openNotif);
+    [Signature("48 89 6C 24 ?? 56 41 56 41 57 48 83 EC ?? 4C 8B F9 0F B6 EA", DetourName = nameof(ShowSharedNotificationDetour))]
+    private Hook<ShowSharedNotificationDelegate>? showSharedNotificationHook;
+
+    // https://github.com/aers/FFXIVClientStructs/blob/main/FFXIVClientStructs/FFXIV/Client/UI/Misc/TofuHelper.cs#L50
+    private delegate void SaveBoardAndPlaySoundDelegate(nint thisPtr, nint packetData, nint boardInfo, uint boardIndexInSharedFolder, uint totalBoardsInSharedFolder);
+    [Signature("E8 ?? ?? ?? ?? 40 80 F5", DetourName = nameof(SaveBoardAndPlaySoundDetour))]
+    private Hook<SaveBoardAndPlaySoundDelegate>? saveBoardAndPlaySoundHook;
+
+    public override void OnEnable() {
+        Services.GameInteropProvider.InitializeFromAttributes(this);
+        showSharedNotificationHook?.Enable();
+        saveBoardAndPlaySoundHook?.Enable();
+    }
+
+    public override void OnDisable() {
+        showSharedNotificationHook?.Dispose();
+        showSharedNotificationHook = null;
+        saveBoardAndPlaySoundHook?.Dispose();
+        saveBoardAndPlaySoundHook = null;
+    }
+
+    private void ShowSharedNotificationDetour(nint thisPtr, bool isNotRealTimeSharing, bool openNotif) { }
+
+    private void SaveBoardAndPlaySoundDetour(nint thisPtr, nint packetData, nint boardInfo, uint boardIndexInSharedFolder, uint totalBoardsInSharedFolder) { }
+}
