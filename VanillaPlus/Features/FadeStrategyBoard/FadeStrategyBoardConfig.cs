f736227b0de251b6978506db252cd1b44c21cce5
MidoriKami <mi_kami@outlook.com>
2026-04-24 23:48:16 -0700

    Remove Fade Strategy Board

--- VanillaPlus/Features/FadeStrategyBoard/FadeStrategyBoardConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using VanillaPlus.Classes;
-
-namespace VanillaPlus.Features.FadeStrategyBoard;
-
-public class FadeStrategyBoardConfig : GameModificationConfig<FadeStrategyBoardConfig> {
-    protected override string FileName => "FadeStrategyBoard";
-
-    public float FadePercentage = 0.8f;
-}
