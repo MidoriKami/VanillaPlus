e9089b1e3d7aa2e1bf73c1cc3553402f2bf2e05d
MidoriKami <mi_kami@outlook.com>
2026-04-25 18:50:09 -0700

    Disable aetheryte on top completely when viewing non territory maps

--- VanillaPlus/Features/ShowAetherytesOnTop/ShowAetherytesOnTop.cs
+++ VanillaPlus/Features/ShowAetherytesOnTop/ShowAetherytesOnTop.cs
@@ -39,6 +39,8 @@ public unsafe class ShowAetherytesOnTop : GameModification {
     }
 
     private void OnMapPostUpdate(AgentEvent type, AgentArgs args) {
+        if (AgentMap.Instance()->SelectedTerritoryId is 0) return;
+        
         if (!controlPreState.HasFlag(KeyStateFlags.Down) && !controlPreState.HasFlag(KeyStateFlags.Held)) {
             ControlKeyState = KeyStateFlags.None;
             args.GetAgentPointer<AgentMap>()->IsControlKeyPressed = false;
