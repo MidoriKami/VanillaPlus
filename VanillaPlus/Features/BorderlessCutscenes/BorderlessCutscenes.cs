c4e6cb9267fdf48360190206b14ed5787603bf3f
MidoriKami <mi_kami@outlook.com>
2026-05-02 16:42:48 -0700

    Add Borderless Cutscenes

---
+++ VanillaPlus/Features/BorderlessCutscenes/BorderlessCutscenes.cs
@@ -0,0 +1,35 @@
+﻿using Dalamud.Utility.Signatures;
+using VanillaPlus.Classes;
+using VanillaPlus.Enums;
+
+namespace VanillaPlus.Features.BorderlessCutscenes;
+
+public class BorderlessCutscenes : GameModification {
+    public override ModificationInfo ModificationInfo => new() {
+        DisplayName = "Borderless Cutscenes",
+        Description = "Removes the letterboxing in cutscenes when using ultrawide displays.",
+        Type = ModificationType.GameBehavior,
+        Authors = [ "goat", "Maple", "MidoriKami" ],
+    };
+
+    [Signature("01 0F 84 ?? ?? ?? ?? 48 8B 0D ?? ?? ?? ?? 0F 57 D2 F3 0F 10 1D ?? ?? ?? ?? 0F 57 C9 48 89 B4 24")]
+    private nint? memoryAddress;
+
+    private MemoryReplacement? jumpPatch;
+
+    public override void OnEnable() {
+        Services.GameInteropProvider.InitializeFromAttributes(this);
+        
+        if (memoryAddress is { } address && memoryAddress != nint.Zero) {
+            jumpPatch = new MemoryReplacement(address, [0x00]);
+            jumpPatch.Enable();
+        }
+    }
+
+    public override void OnDisable() {
+        jumpPatch?.Dispose();
+        jumpPatch = null;
+        
+        memoryAddress = null;
+    }
+}
