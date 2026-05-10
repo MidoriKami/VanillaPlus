472b2e95ccd95b0c127e47c0b919adb52b37849e
MidoriKami <mi_kami@outlook.com>
2026-05-09 19:01:23 -0700

    Consider this a newly opened can of worms

--- VanillaPlus/DevFeatures/DebugCustomAddon/DebugAddon.cs
+++ VanillaPlus/DevFeatures/DebugCustomAddon/DebugAddon.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Textures.TextureWraps;
+using Dalamud.Interface.Utility;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit;
+using KamiToolKit.Nodes;
 
 namespace VanillaPlus.DevFeatures.DebugCustomAddon;
 
@@ -10,8 +14,49 @@ namespace VanillaPlus.DevFeatures.DebugCustomAddon;
 /// </summary>
 public class DebugAddon : NativeAddon {
 
-    protected override unsafe void OnSetup(AtkUnitBase* addon, Span<AtkValue> atkValueSpan) {
+    private ImGuiImageNode? customImageNode;
+    private IDrawListTextureWrap? textureWrap;
 
+    protected override unsafe void OnSetup(AtkUnitBase* addon, Span<AtkValue> atkValueSpan) {
+        SetWindowSize(new Vector2(750.0f, 400.0f));
+
+        customImageNode = new ImGuiImageNode {
+            Size = ContentSize,
+            Position = ContentStartPosition,
+        };
+        customImageNode.AttachNode(this);
+
+        Services.PluginInterface.UiBuilder.Draw += InitializeDraw;
+    }
+
+    public override void Dispose() {
+        Services.PluginInterface.UiBuilder.Draw -= InitializeDraw;
+
+        base.Dispose();
+    }
+
+    private void InitializeDraw() {
+        if (textureWrap is null) {
+            using var drawData = BufferBackedImDrawData.Create();
+
+            var size = new Vector2(64.0f, 64.0f);
+
+            // drawData.ListPtr.AddCircleFilled(size / 2.0f, 32.0f, ImGui.GetColorU32(KnownColor.ForestGreen.Vector()));
+
+            drawData.DataPtr.DisplaySize = size;
+            drawData.UpdateDrawDataStatistics();
+
+            textureWrap = Services.TextureProvider.CreateDrawListTexture("VanillaPlusTest");
+
+            textureWrap.Size = size;
+
+            // textureWrap.Draw(drawData.DataPtr);
+            textureWrap.ResizeAndDrawWindow("", Vector2.One);
+            customImageNode?.LoadTexture(textureWrap);
+        }
+
+        textureWrap.ResizeAndDrawWindow("", Vector2.One);
+        customImageNode?.TextureSize = textureWrap.Size;
     }
 }
 #endif
--- VanillaPlus/DevFeatures/DebugImGuiWindow/DebugWindow.cs
+++ VanillaPlus/DevFeatures/DebugImGuiWindow/DebugWindow.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Drawing;
 using System.Numerics;
+using Dalamud.Bindings.ImGui;
+using Dalamud.Interface;
+using Dalamud.Interface.Textures.TextureWraps;
+using Dalamud.Interface.Utility;
 using Dalamud.Interface.Windowing;
 
 namespace VanillaPlus.DevFeatures.DebugImGuiWindow;
@@ -10,6 +15,8 @@ namespace VanillaPlus.DevFeatures.DebugImGuiWindow;
 /// </summary>
 public class DebugWindow : Window, IDisposable {
 
+    private IDrawListTextureWrap? textureWrap;
+
     public DebugWindow() : base("Vanilla Plus Debug Window") {
         SizeConstraints = new WindowSizeConstraints {
             MinimumSize = new Vector2(500.0f, 500.0f),
@@ -18,11 +25,28 @@ public class DebugWindow : Window, IDisposable {
     }
 
     public void Dispose() {
-
+        textureWrap?.Dispose();
     }
 
     public override void Draw() {
+        if (textureWrap is null) {
+            using var drawData = BufferBackedImDrawData.Create();
 
+            var viewport = ImGui.GetMainViewport();
+
+            drawData.ListPtr.AddRectFilled(Vector2.Zero, viewport.Size, ImGui.GetColorU32(KnownColor.ForestGreen.Vector()));
+
+            drawData.DataPtr.DisplaySize = viewport.Size;
+            drawData.UpdateDrawDataStatistics();
+
+            textureWrap = Services.TextureProvider.CreateDrawListTexture("VanillaPlusTest");
+
+            textureWrap.Size = viewport.Size;
+
+            textureWrap.Draw(drawData.DataPtr);
+        }
+
+        ImGui.Image(textureWrap.Handle, textureWrap.Size);
     }
 }
 #endif
