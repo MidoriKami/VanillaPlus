7e5f85d19c3e852a73cfa4e15c9560d6a54526d2
MidoriKami <mi_kami@outlook.com>
2026-05-22 12:06:35 -0700

    Add Debug Hook Helper

---
+++ VanillaPlus/DevFeatures/DebugHook/DebugHook.cs
@@ -0,0 +1,44 @@
+﻿// ReSharper disable RedundantUnsafeContext
+
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using VanillaPlus.Classes;
+using VanillaPlus.Enums;
+
+namespace VanillaPlus.DevFeatures.DebugHook;
+
+#if DEBUG
+/// <summary>
+/// Debug Game Modification for use with playing around with ideas, DO NOT commit changes to this file
+/// </summary>
+public unsafe class DebugHook : GameModification {
+    public override ModificationInfo ModificationInfo => new() {
+        DisplayName = "Debug Hook",
+        Description = "Templated Helper for debugging hooks.",
+        Type = ModificationType.Debug,
+        Authors = ["YourNameHere"],
+    };
+
+    private delegate void HookDelegate();
+
+    [Signature("", DetourName = nameof(HookDetour))]
+    private Hook<HookDelegate>? hook;
+
+    public override void OnEnable() {
+        Services.GameInteropProvider.InitializeFromAttributes(this);
+
+        hook?.Enable();
+    }
+
+    public override void OnDisable() {
+        hook?.Disable();
+        hook = null;
+    }
+
+    private void HookDetour() {
+
+        hook!.Original();
+
+    }
+}
+#endif
