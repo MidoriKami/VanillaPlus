06db0b04756a77a5bf193a1d48f6fb0f27b9e53d
MidoriKami <mi_kami@outlook.com>
2026-05-31 11:21:16 -0700

    Touchups

--- VanillaPlus/Features/AetherwellProgress/AetherwellProgress.cs
+++ VanillaPlus/Features/AetherwellProgress/AetherwellProgress.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Threading.Tasks;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
 using KamiToolKit.Controllers;
@@ -11,7 +12,7 @@ using VanillaPlus.Enums;
 
 namespace VanillaPlus.Features.AetherwellProgress;
 
-public unsafe class AetherwellProgress : GameModification {
+public class AetherwellProgress : GameModification {
     public override ModificationInfo ModificationInfo => new() {
         DisplayName = Strings.ModificationDisplay_AetherwellProgress,
         Description = Strings.ModificationDescription_AetherwellProgress,
@@ -21,49 +22,57 @@ public unsafe class AetherwellProgress : GameModification {
 
     public override string ImageName => "AetherwellProgress.png";
 
-    private readonly Vector2 baseUV = new(0, 276);
+    private readonly Vector2 baseUv = new(0, 276);
     private readonly Vector2 baseSize = new(92, 92);
     private readonly Vector2 basePosition = new(26, 26);
-    private readonly List<uint> buttonIDs = [2, 3, 4, 5];
 
     private AddonController? addonController;
-    private readonly List<SimpleImageNode> nodes = [];
+    private List<SimpleImageNode>? nodes;
 
-    public override void OnEnable() {
-        addonController = new AddonController {
-            AddonName = "MKDRelicGrowth",
-            OnSetup = OnAddonSetup,
-            OnRefresh = OnAddonRefresh,
-            OnFinalize = OnAddonFinalize,
-        };
-        addonController.Enable();
+    public override async Task OnEnableAsync() {
+        nodes = [];
+
+        unsafe {
+            addonController = new AddonController {
+                AddonName = "MKDRelicGrowth",
+                OnSetup = OnAddonSetup,
+                OnRefresh = OnAddonRefresh,
+                OnFinalize = OnAddonFinalize,
+            };
+        }
+
+        await Services.Framework.Run(addonController.Enable);
     }
 
-    public override void OnDisable() {
-        addonController?.Dispose();
+    public override async Task OnDisableAsync() {
+        await Services.Framework.Run(() => addonController?.Dispose());
         addonController = null;
-        // the nodes list is cleared by addonController.OnFinalize, so we can ignore it here
+
+        nodes = null;
     }
 
-    private void OnAddonSetup(AtkUnitBase* addon) {
-        foreach (var buttonId in buttonIDs) {
-            var btn = addon->GetComponentButtonById(buttonId);
-            if (btn == null) return;
-            SetupNode(btn);
+    private unsafe void OnAddonSetup(AtkUnitBase* addon) {
+        foreach (var buttonId in (List<uint>) [2, 3, 4, 5]) {
+            var buttonComponent = addon->GetComponentButtonById(buttonId);
+            if (buttonComponent is null) return;
+
+            SetupNode(buttonComponent);
         }
     }
 
-    private void SetupNode(AtkComponentButton* btn) {
-        var existingImageNode = btn->GetImageNodeById(17);
-        if (existingImageNode == null) return;
-        existingImageNode->SetWidth(0);
-        existingImageNode->SetHeight(0);
+    private unsafe void SetupNode(AtkComponentButton* buttonComponent) {
+        if (nodes is null) return;
+
+        var existingImageNode = buttonComponent->GetImageNodeById(17);
+        if (existingImageNode is null) return;
+
+        existingImageNode->AtkResNode.Size = Vector2.Zero;
 
         var imageNode = new SimpleImageNode {
             Position = basePosition,
             Size = baseSize,
-            Origin = baseSize/2,
-            TextureCoordinates = baseUV,
+            Origin = baseSize / 2.0f,
+            TextureCoordinates = baseUv,
             TexturePath = "ui/uld/MKDRelicGrowth2.tex",
             TextureSize = baseSize,
         };
@@ -73,45 +82,49 @@ public unsafe class AetherwellProgress : GameModification {
         nodes.Add(imageNode);
     }
 
-    private void OnAddonRefresh(AtkUnitBase* addon) {
-        foreach (var (i, node) in nodes.Index()) {
-            if (!long.TryParse(addon->AtkValuesSpan[i * 4].String.ToString(), out var progress))
+    private unsafe void OnAddonRefresh(AtkUnitBase* addon) {
+        if (nodes is null) return;
+
+        foreach (var (index, node) in nodes.Index()) {
+            if (!long.TryParse(addon->AtkValuesSpan[index * 4].String.ToString(), out var progress))
                 continue;
-            if (!long.TryParse(addon->AtkValuesSpan[(i * 4) + 1].String.ToString(), out var max))
+
+            if (!long.TryParse(addon->AtkValuesSpan[index * 4 + 1].String.ToString(), out var max))
                 continue;
-            var pct = float.Clamp((float)progress / max, 0, 1);
+
+            var progressPercent = float.Clamp((float)progress / max, 0, 1);
             var position = basePosition;
-            var size = baseSize with { Y = float.Floor(baseSize.Y * pct) };
+            var size = baseSize with { Y = float.Floor(baseSize.Y * progressPercent) };
             var shift = baseSize.Y - size.Y;
-            var uv = baseUV with { Y = baseUV.Y + shift };
+            var textureCoordinate = baseUv with { Y = baseUv.Y + shift };
 
             position.Y += shift;
             node.Position = position;
             node.Size = size;
-            node.TextureCoordinates = uv;
+            node.TextureCoordinates = textureCoordinate;
             node.TextureSize = size;
         }
     }
 
-    private void OnAddonFinalize(AtkUnitBase* addon) {
-        foreach (var n in nodes) {
-            n.DetachNode();
+    private unsafe void OnAddonFinalize(AtkUnitBase* addon) {
+        foreach (var n in nodes ?? []) {
             n.Dispose();
         }
-        nodes.Clear();
+        nodes?.Clear();
 
-        foreach (var buttonId in buttonIDs) {
-            var btn = addon->GetComponentButtonById(buttonId);
-            if (btn == null) continue;
-            RestoreNode(btn);
+        foreach (var buttonId in (List<uint>) [2, 3, 4, 5]) {
+            var buttonComponent = addon->GetComponentButtonById(buttonId);
+            if (buttonComponent is null) continue;
+
+            RestoreNode(buttonComponent);
         }
     }
 
-    private void RestoreNode(AtkComponentButton* btn) {
+    private unsafe void RestoreNode(AtkComponentButton* btn) {
         var imageNode = btn->GetImageNodeById(17);
-        if (imageNode == null) return;
-        imageNode->SetWidth((ushort)baseSize.X);
-        imageNode->SetHeight((ushort)baseSize.Y);
+        if (imageNode is null) return;
+
+        imageNode->AtkResNode.Size = new Vector2(baseSize.X, baseSize.Y);
     }
 
     private static Timeline AnimationTimeline() {
--- VanillaPlus/Resources/Strings.Designer.cs
+++ VanillaPlus/Resources/Strings.Designer.cs
@@ -2456,15 +2456,6 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Adds a button in the enemy list to reset a Striking Dummy&apos;s enmity..
-        /// </summary>
-        internal static string ModificationDescription_ResetDummyEnmity {
-            get {
-                return ResourceManager.GetString("ModificationDescription_ResetDummyEnmity", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Automatically resets the inventory to the first tab when opened..
         /// </summary>
         internal static string ModificationDescription_ResetInventoryTab {
@@ -3117,15 +3108,6 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Reset Dummy Enmity.
-        /// </summary>
-        internal static string ModificationDisplay_ResetDummyEnmity {
-            get {
-                return ResourceManager.GetString("ModificationDisplay_ResetDummyEnmity", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Reset Inventory Tab.
         /// </summary>
         internal static string ModificationDisplay_ResetInventoryTab {
@@ -3707,15 +3689,6 @@ namespace Resources {
         internal static string RecentlyLootedWindow_Title {
             get {
                 return ResourceManager.GetString("RecentlyLootedWindow_Title", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to [VanillaPlus] Reset Enmity.
-        /// </summary>
-        internal static string ResetDummyEnmity_ResetEnmityToolTip {
-            get {
-                return ResourceManager.GetString("ResetDummyEnmity_ResetEnmityToolTip", resourceCulture);
             }
         }
         
