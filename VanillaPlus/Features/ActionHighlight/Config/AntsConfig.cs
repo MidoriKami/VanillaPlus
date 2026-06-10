178f3102a4b27f887e54563e3e5caab62b6f2aa5
MidoriKami <mi_kami@outlook.com>
2026-06-09 22:50:52 -0700

    Fix ants migration

--- VanillaPlus/Features/ActionHighlight/Config/AntsConfig.cs
+++ VanillaPlus/Features/ActionHighlight/Config/AntsConfig.cs
@@ -21,27 +21,27 @@ public class AntsConfig : GameModificationConfig<AntsConfig> {
 
     public List<AntsClassJobConfig> ClassJobConfigs = [];
 
-    // public AntsActionSetting? GetActionSetting(uint actionId) {
-    //
-    //
-    //     return actionSetting;
-    // }
-
     protected override bool TryMigrateConfig(int? fileVersion, JObject jObject) {
         switch (fileVersion) {
             case 1:
                 throw new NotSupportedException("Attempted to migrate a ActionHighlight config that was too old, only v2 -> v3 is currently supported.");
 
             case 2:
-                var oldActionSettings = jObject["ActionSettings"]?.ToObject<Dictionary<uint, AntsActionSetting>>();
-                if (oldActionSettings is null) return false;
+                var actionSettingsObj = jObject["ActionSettings"]?.ToObject<JObject>();
+                if (actionSettingsObj is null) return false;
 
                 List<AntsClassJobConfig> newEntries = [];
 
-                foreach (var (actionId, actionSettings) in oldActionSettings) {
+                foreach (var property in actionSettingsObj.Properties()) {
+                    if (!uint.TryParse(property.Name, out var actionId)) continue;
+
+                    var actionSettings = property.Value.ToObject<AntsActionSetting>();
+                    if (actionSettings is null) continue;
+
                     var actionInfo = Services.DataManager.GetExcelSheet<Action>().GetRow(actionId);
 
                     var classJobEntry = newEntries.FirstOrDefault(entry => entry.ClassJobId == actionInfo.ClassJob.RowId);
+
                     if (classJobEntry is null) {
                         newEntries.Add(new AntsClassJobConfig {
                             ClassJobId = actionInfo.ClassJob.RowId,
@@ -54,6 +54,8 @@ public class AntsConfig : GameModificationConfig<AntsConfig> {
                         classJobEntry.ActionSettings.Add(actionSettings);
                     }
                 }
+
+                ClassJobConfigs = newEntries;
                 return true;
         }
 
