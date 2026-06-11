16947a1f0a1caefb286502bfb394c48c1770a318
MidoriKami <mi_kami@outlook.com>
2026-06-10 23:36:11 -0700

    Rework ItemStack

--- VanillaPlus/Classes/ItemStack.cs
+++ VanillaPlus/Classes/ItemStack.cs
@@ -1,27 +1,15 @@
 ﻿using System.Text.RegularExpressions;
 using FFXIVClientStructs.FFXIV.Client.Game;
-using VanillaPlus.Enums;
 
 namespace VanillaPlus.Classes;
 
 public record ItemStack(InventoryItem Item, int Quantity) {
 
-    public static int Comparison(ItemStack left, ItemStack right, InventoryFilterMode filterMode) {
-        var leftItem = Services.DataManager.GetItem(left.Item.ItemId);
-        var rightItem = Services.DataManager.GetItem(right.Item.ItemId);
-
-        var result = filterMode switch {
-            InventoryFilterMode.Alphabetical => string.CompareOrdinal(leftItem.Name.ToString(), rightItem.Name.ToString()),
-            InventoryFilterMode.ClassJobLevel => rightItem.LevelEquip.CompareTo(leftItem.LevelEquip),
-            InventoryFilterMode.ItemLevel => rightItem.LevelItem.RowId.CompareTo(leftItem.LevelItem.RowId),
-            InventoryFilterMode.Rarity => rightItem.Rarity.CompareTo(leftItem.Rarity),
-            InventoryFilterMode.ItemId => rightItem.RowId.CompareTo(leftItem.RowId),
-            InventoryFilterMode.ItemCategory => rightItem.ItemUICategory.RowId.CompareTo(leftItem.ItemUICategory.RowId),
-            InventoryFilterMode.Quantity => left.Quantity.CompareTo(right.Quantity),
-            _ => string.CompareOrdinal(leftItem.Name.ToString(), rightItem.Name.ToString()),
-        };
-
-        return result is 0 ? string.CompareOrdinal(leftItem.Name.ToString(), rightItem.Name.ToString()) : result;
+    public string ItemName {
+        get {
+            var inventoryItem = Item;
+            return inventoryItem.Name.ToString();
+        }
     }
 
     public static bool IsMatch(ItemStack itemStack, string searchTerms) {
