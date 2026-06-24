65d2988302188f920c972369cf623e49bf5d44c7
MidoriKami <mi_kami@outlook.com>
2026-06-24 14:35:24 -0700

    New extensions for Castbar Countdown

---
+++ VanillaPlus/Extensions/BattleCharaExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Dalamud.Game.ClientState.Objects.Types;
+
+namespace VanillaPlus.Extensions;
+
+/// <summary>
+/// Extension methods for IBattleChara
+/// </summary>
+public static class BattleCharaExtensions {
+    extension(IBattleChara battleChara) {
+        public string GetCastTimeString {
+            get {
+                if (!battleChara.IsValid()) return string.Empty;
+                if (battleChara.EntityId is 0xE0000000) return string.Empty;
+                if (battleChara.CurrentCastTime >= battleChara.TotalCastTime) return string.Empty;
+
+                return (battleChara.TotalCastTime - battleChara.CurrentCastTime).ToString("00.00", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ VanillaPlus/Extensions/ObjectTableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Plugin.Services;
+
+namespace VanillaPlus.Extensions;
+
+/// <summary>
+/// Extension methods for dalamud's IObjectTable service.
+/// </summary>
+public static class ObjectTableExtensions {
+    extension(IObjectTable objectTable) {
+        public IBattleChara? GetBattleChara(uint entityId)
+            => objectTable.CharacterManagerObjects.FirstOrDefault(obj => obj.EntityId == entityId) as IBattleChara;
+    }
+}
