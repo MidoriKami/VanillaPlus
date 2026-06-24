2e19bb560224a2c2a16555c459d80fc4f8ed56f4
Abyeon <17396444+Abyeon@users.noreply.github.com>
2026-06-24 03:29:44 -0400

    Revert "regenerate strings"
    This reverts commit 57e9e9331ee92a4c4c3d1c9d4b5f2013ac4750ae.

--- VanillaPlus/Resources/Strings.Designer.cs
+++ VanillaPlus/Resources/Strings.Designer.cs
@@ -2180,7 +2180,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Disabled.
+        ///   Looks up a localized string similar to Disable.
         /// </summary>
         internal static string ModificationBrowser_TabDisabled {
             get {
@@ -2189,7 +2189,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enabled.
+        ///   Looks up a localized string similar to Enable.
         /// </summary>
         internal static string ModificationBrowser_TabEnabled {
             get {
