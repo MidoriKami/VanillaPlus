57e9e9331ee92a4c4c3d1c9d4b5f2013ac4750ae
Abyeon <17396444+Abyeon@users.noreply.github.com>
2026-06-24 03:25:47 -0400

    regenerate strings

--- VanillaPlus/Resources/Strings.Designer.cs
+++ VanillaPlus/Resources/Strings.Designer.cs
@@ -2181,7 +2181,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Disable.
+        ///   Looks up a localized string similar to Disabled.
         /// </summary>
         internal static string ModificationBrowser_TabDisabled {
             get {
@@ -2190,7 +2190,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enable.
+        ///   Looks up a localized string similar to Enabled.
         /// </summary>
         internal static string ModificationBrowser_TabEnabled {
             get {
