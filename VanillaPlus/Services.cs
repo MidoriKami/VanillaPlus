7fd385e1caea4ce48172e381061cae04e8aecbeb
MidoriKami <mi_kami@outlook.com>
2026-07-17 11:57:29 -0700

    Comment

--- VanillaPlus/Services.cs
+++ VanillaPlus/Services.cs
@@ -7,8 +7,12 @@ namespace VanillaPlus;
 /// Generic service getter, allows you to get any dalamud service you need via the interface typename, for example IAddonLifecycle or IPluginLog.
 /// </summary>
 public static class Service<T> where T : class, IDalamudService {
-    private static T? ServiceInstance => field ??= VanillaPlus.PluginInterface.GetService(typeof(T)) as T;
+    private static T? ServiceInstance
+        => field ??= VanillaPlus.PluginInterface.GetService(typeof(T)) as T;
 
+    /// <summary>
+    /// Gets a reference to the dalamud service via type.
+    /// </summary>
     public static T Get()
         => ServiceInstance ?? throw new InvalidOperationException($"Service {typeof(T).Name} not found.");
 }
