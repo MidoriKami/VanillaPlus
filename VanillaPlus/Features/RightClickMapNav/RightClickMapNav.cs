9d25cb5cd5f3fdba1ba2aca4813262ecb8e8d88f
MidoriKami <mi_kami@outlook.com>
2026-07-31 22:12:07 -0700

    Implement Right-Click Map Nav
    closes #326

---
+++ VanillaPlus/Features/RightClickMapNav/RightClickMapNav.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using VanillaPlus.Classes;
+using VanillaPlus.Enums;
+using AgentId = Dalamud.Game.Agent.AgentId;
+
+namespace VanillaPlus.Features.RightClickMapNav;
+
+public class RightClickMapNav : GameModification {
+    public override ModificationInfo ModificationInfo => new() {
+        DisplayName = "Right-Click Map Nav",
+        Description = "Allows you to navigate 'up' a layer by right clicking on the map.",
+        Type = ModificationType.GameBehavior,
+        Authors = ["MidoriKami"],
+    };
+
+    public override Task OnEnableAsync() {
+        IAddonLifecycle.Get().RegisterListener(AddonEvent.PreReceiveEvent, "AreaMap", OnAreaMapReceiveEvent);
+
+        IAgentLifecycle.Get().LogAgent(AgentId.Map);
+
+        return Task.CompletedTask;
+    }
+
+    public override Task OnDisableAsync() {
+        IAddonLifecycle.Get().UnregisterListener(OnAreaMapReceiveEvent);
+
+        return Task.CompletedTask;
+    }
+
+    private static unsafe void OnAreaMapReceiveEvent(AddonEvent type, AddonArgs args) {
+        if (args is not AddonReceiveEventArgs receiveEventArgs) return;
+
+        var addon = args.GetAddon<AddonAreaMap>();
+        if (!addon->MapUpButton->OwnerNode->NodeFlags.HasFlag(NodeFlags.Enabled)) return;
+
+        var eventData = (AtkEventData*)receiveEventArgs.AtkEventData;
+        if (!eventData->IsRightClick) return;
+
+        AgentMap.Instance()->AgentInterface.SendCommand(0, [5]);
+
+        IPluginLog.Get().Debug("RightClickMapNav prevented map right click to nav up.");
+        args.PreventOriginal();
+    }
+}
