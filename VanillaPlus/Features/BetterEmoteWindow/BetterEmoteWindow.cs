8f57b62e54d17bb69591f9af992dbad065567f00
MidoriKami <mi_kami@outlook.com>
2026-08-28 09:05:41 -0700

    Cleanup better emote window

--- VanillaPlus/Features/BetterEmoteWindow/BetterEmoteWindow.cs
+++ VanillaPlus/Features/BetterEmoteWindow/BetterEmoteWindow.cs
@@ -1,4 +1,7 @@
+using System.Numerics;
 using System.Threading.Tasks;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Controllers;
 using VanillaPlus.Classes;
 using VanillaPlus.Enums;
 using VanillaPlus.Features.BetterEmoteWindow.Controllers;
@@ -15,12 +18,19 @@ public class BetterEmoteWindow : GameModification {
 
     public override string ImageName => "BetterEmoteWindow.png";
 
-    private EmoteWindowLayoutController? layoutController;
     private EmoteTooltipController? tooltipController;
+    private AddonController? emoteController;
 
     public override async Task OnEnableAsync() {
-        layoutController = new EmoteWindowLayoutController();
-        await layoutController.EnableAsync();
+        unsafe {
+            emoteController = new AddonController {
+                AddonName = "Emote",
+                OnSetup = OnEmoteSetup,
+                OnFinalize = OnEmoteFinalize,
+            };
+        }
+
+        await emoteController.EnableAsync();
 
         tooltipController = new EmoteTooltipController();
         tooltipController.Enable();
@@ -30,7 +40,61 @@ public class BetterEmoteWindow : GameModification {
         await tooltipController.DisposeAsyncSafe();
         tooltipController = null;
 
-        await layoutController.DisposeAsyncSafe();
-        layoutController = null;
+        await emoteController.DisposeAsyncSafe();
+        emoteController = null;
+    }
+
+    private unsafe void OnEmoteSetup(AtkUnitBase* addon) {
+
+        // Hide Description Panel
+        var descriptionPanel = addon->GetNodeById(11);
+        if (descriptionPanel is not null) {
+            descriptionPanel->Visible = false;
+        }
+
+        // Move category sections upwards
+        foreach (var nodeId in new[] { 4u, 16u, 21u, 41u }) {
+            var categoryNode = addon->GetNodeById(nodeId);
+            if (categoryNode is not null) {
+                categoryNode->Position -= new Vector2(0.0f, descriptionPanel->Height - 5.0f);
+            }
+        }
+
+        // Adjust list size
+        var listNode = addon->GetComponentListById(4);
+        if (listNode is not null) {
+            listNode->OwnerNode->AtkResNode.Size += new Vector2(0.0f, descriptionPanel->Height - 5.0f);
+            listNode->SetVisibleRowCount((short) (listNode->VisibleRowCount + 3));
+        }
+
+        // Trigger layout update
+        addon->Size += new Vector2(0.0f, 5.0f);
+    }
+
+    private unsafe void OnEmoteFinalize(AtkUnitBase* addon) {
+
+        // Restore Description Panel
+        var descriptionPanel = addon->GetNodeById(11);
+        if (descriptionPanel is not null) {
+            descriptionPanel->Visible = true;
+        }
+
+        // Move category sections back
+        foreach (var nodeId in new[] { 4u, 16u, 21u, 41u }) {
+            var categoryNode = addon->GetNodeById(nodeId);
+            if (categoryNode is not null) {
+                categoryNode->Position += new Vector2(0.0f, descriptionPanel->Height - 5.0f);
+            }
+        }
+
+        // Restore list size
+        var listNode = addon->GetComponentListById(4);
+        if (listNode is not null) {
+            listNode->OwnerNode->AtkResNode.Size -= new Vector2(0.0f, descriptionPanel->Height - 5.0f);
+            listNode->SetVisibleRowCount((short) (listNode->VisibleRowCount - 3));
+        }
+
+        // Trigger layout update
+        addon->Size -= new Vector2(0.0f, 5.0f);
     }
 }
